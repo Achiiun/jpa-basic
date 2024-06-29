7cc32f1d3c40b2ae5c6c1ea9f5bf64b6795a3f5d
Achiiun <rlduwl0404@gmail.com>
2024-06-29 15:33:12 +0900

    JPA 프로젝트 생성 및 개발

---
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,39 @@
+package hellojpa;
+
+import jakarta.persistence.*;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("helloA");
+
+            em.persist(member);
+
+            Member findMember = em.find(Member.class, member.getId());
+            System.out.println("findMember = " + findMember.getId());
+            System.out.println("findMember = " + findMember.getName());
+
+            findMember.setName("helloJPA");
+            System.out.println("=============update=============");
+            System.out.println("findMember = " + findMember.getId());
+            System.out.println("findMember = " + findMember.getName());
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
