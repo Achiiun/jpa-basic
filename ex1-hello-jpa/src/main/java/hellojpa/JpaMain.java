7c2e712121bd931a729e5931f44fc4d0e3a76b6c
Achiiun <rlduwl0404@gmail.com>
2024-07-01 22:09:07 +0900

    엔티티 매핑 - 필드와 컬럼 매핑

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -13,20 +13,27 @@ public class JpaMain {
         tx.begin();
 
         try {
+//            Member member = new Member();
+//            member.setId(1L);
+//            member.setName("helloA");
+//
+//            em.persist(member);
+//
+//            Member findMember = em.find(Member.class, member.getId());
+//            System.out.println("findMember = " + findMember.getId());
+//            System.out.println("findMember = " + findMember.getName());
+//
+//            findMember.setName("helloJPA");
+//            System.out.println("=============update=============");
+//            System.out.println("findMember = " + findMember.getId());
+//            System.out.println("findMember = " + findMember.getName());
+
             Member member = new Member();
-            member.setId(1L);
-            member.setName("helloA");
+            member.setId(2L);
+            member.setUsername("A");
+            member.setRoleType(RoleType.USER);
 
             em.persist(member);
-
-            Member findMember = em.find(Member.class, member.getId());
-            System.out.println("findMember = " + findMember.getId());
-            System.out.println("findMember = " + findMember.getName());
-
-            findMember.setName("helloJPA");
-            System.out.println("=============update=============");
-            System.out.println("findMember = " + findMember.getId());
-            System.out.println("findMember = " + findMember.getName());
 
             tx.commit();
         } catch (Exception e) {
