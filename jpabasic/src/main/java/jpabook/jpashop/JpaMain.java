5e000f5c0461829eeee473ac4710cbcc81db053e
Achiiun <rlduwl0404@gmail.com>
2024-07-06 19:57:48 +0900

    실전예제4 - 상속관계 매핑

--- jpabasic/src/main/java/jpabook/jpashop/JpaMain.java
+++ jpabasic/src/main/java/jpabook/jpashop/JpaMain.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import jpabook.jpashop.domain.Book;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
 
@@ -17,13 +18,11 @@ public class JpaMain {
     tx.begin();
 
     try {
-//      Order order = new Order();
-//      em.persist(order);
-//
-//      OrderItem orderItem = new OrderItem();
-//      orderItem.setOrder(order);
+      Book book = new Book();
+      book.setName("JPA");
+      book.setAuthor("김영한");
 
-//      em.persist(orderItem);
+      em.persist(book);
 
       tx.commit();
     } catch (Exception e) {
