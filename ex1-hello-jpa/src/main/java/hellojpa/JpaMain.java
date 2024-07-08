d698bacd1dcae9caa777e6305cd34c16d02d6f09
Achiiun <rlduwl0404@gmail.com>
2024-07-08 22:15:05 +0900

    값 타입 - 기본값, 임베디드, 값 타입 컬렉션

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import org.hibernate.Hibernate;
 
 import java.util.List;
+import java.util.Set;
 
 public class JpaMain {
 
@@ -51,20 +52,67 @@ public class JpaMain {
 
 //            System.out.println("refMember == findMember: " + (refMember == findMember));
 
-            Child child1 = new Child();
-            Child child2 = new Child();
+//            Child child1 = new Child();
+//            Child child2 = new Child();
+//
+//            Parent parent = new Parent();
+//            parent.addChild(child1);
+//            parent.addChild(child2);
+//
+//            em.persist(parent);
+//
+//            em.flush();
+//            em.clear();
+//
+//            Parent findParent = em.find(Parent.class, parent.getId());
+//            findParent.getChildList().remove(0);
 
-            Parent parent = new Parent();
-            parent.addChild(child1);
-            parent.addChild(child2);
+//            Address address = new Address("city", "street", "10000");
+//
+//            Member member1 = new Member();
+//            member1.setUsername("hello");
+//            member1.setHomeAddress(address);
+//            em.persist(member1);
+//
+//            Address copyAddress = new Address(address.getCity(), address.getStreet(), address.getZipcode());
+//
+//            Member member2 = new Member();
+//            member2.setUsername("hello");
+//            member2.setHomeAddress(copyAddress);
+//            em.persist(member2);
 
-            em.persist(parent);
+//            member1.getHomeAddress().setCity("newCity");
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setHomeAddress(new Address("homeCity", "street", "10000"));
+
+            member.getFavoriteFoods().add("치킨");
+            member.getFavoriteFoods().add("족발");
+            member.getFavoriteFoods().add("피자");
+
+            member.getAddressHistory().add(new AddressEntity("old1", "street", "10000"));
+            member.getAddressHistory().add(new AddressEntity("old2", "street", "10000"));
+
+            em.persist(member);
 
             em.flush();
             em.clear();
 
-            Parent findParent = em.find(Parent.class, parent.getId());
-            findParent.getChildList().remove(0);
+            System.out.println("===================START=================");
+            Member findMember = em.find(Member.class, member.getId());
+
+            //homeCity -> newCity
+            Address a = findMember.getHomeAddress();
+            findMember.setHomeAddress(new Address("newCity", a.getStreet(), a.getZipcode()));
+
+            //치킨 -> 한식
+            findMember.getFavoriteFoods().remove("치킨");
+            findMember.getFavoriteFoods().add("한식");
+
+            //old1 -> newCity1
+//            findMember.getAddressHistory().remove(new Address("old1", "street", "10000"));
+//            findMember.getAddressHistory().add(new Address("newCity1", "street", "10000"));
 
             tx.commit();
         } catch (Exception e) {
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -2,9 +2,7 @@ package hellojpa;
 
 import jakarta.persistence.*;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 @Entity
 public class Member extends BaseEntity{
@@ -17,18 +15,36 @@ public class Member extends BaseEntity{
   @Column(name = "USERNAME")
   private String username;
 
-  @ManyToOne(fetch = FetchType.LAZY) //지연로딩
-  @JoinColumn(name = "TEAM_ID")
-  private Team team;
+  //기간
+  @Embedded
+  private Period workPeriod;
 
+  //주소
+  @Embedded
+  private Address homeAddress;
 
-  @OneToOne
-  @JoinColumn(name = "LOCKER_ID")
-  private Locker locker;
+  @ElementCollection
+  @CollectionTable(name = "FAVORITE_FOOD", joinColumns =
+  @JoinColumn(name = "MEMBER_ID"))
+  @Column(name = "FOOD_NAME")
+  private Set<String> favoriteFoods = new HashSet<>();
 
-  @OneToMany(mappedBy = "member")
-  private List<MemberProduct> memberProducts = new ArrayList<>();
+//  @ElementCollection
+//  @CollectionTable(name = "ADDRESS", joinColumns =
+//  @JoinColumn(name = "MEMBER_ID"))
+//  private List<Address> addressHistory = new ArrayList<>();
 
+  @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+  @JoinColumn(name = "MEMBER_ID")
+  private List<AddressEntity> addressHistory = new ArrayList<>();
+
+//  @Embedded
+//  @AttributeOverrides({
+//    @AttributeOverride(name = "city", column = @Column(name = "WORK_CITY")),
+//    @AttributeOverride(name = "street", column = @Column(name = "WORK_STREET")),
+//    @AttributeOverride(name = "zipcode", column = @Column(name = "WORK_ZIPCODE"))
+//  })
+//  private Address workAddress;
 
   public Long getId() {
     return id;
@@ -46,11 +62,39 @@ public class Member extends BaseEntity{
     this.username = username;
   }
 
-  public Team getTeam() {
-    return team;
+  public Period getWorkPeriod() {
+    return workPeriod;
   }
 
-  public void setTeam(Team team) {
-    this.team = team;
+  public void setWorkPeriod(Period workPeriod) {
+    this.workPeriod = workPeriod;
+  }
+
+  public Address getHomeAddress() {
+    return homeAddress;
+  }
+
+  public void setHomeAddress(Address homeAddress) {
+    this.homeAddress = homeAddress;
+  }
+
+  public Set<String> getFavoriteFoods() {
+    return favoriteFoods;
+  }
+
+  public void setFavoriteFoods() {
+    this.favoriteFoods = favoriteFoods;
+  }
+
+  public void setFavoriteFoods(Set<String> favoriteFoods) {
+    this.favoriteFoods = favoriteFoods;
+  }
+
+  public List<AddressEntity> getAddressHistory() {
+    return addressHistory;
+  }
+
+  public void setAddressHistory(List<AddressEntity> addressHistory) {
+    this.addressHistory = addressHistory;
   }
 }
